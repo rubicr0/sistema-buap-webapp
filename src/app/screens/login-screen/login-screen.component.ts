d27924c049a3ecc92a2e1fa3c353079c41e8723c
Luis Yael Mendez <luisyaelms@gmail.com>
2024-09-10 13:27:41 -0600

    Terminando login
    Se valida la función de mostrar contraseña.

--- src/app/screens/login-screen/login-screen.component.ts
+++ src/app/screens/login-screen/login-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'app-login-screen',
@@ -26,6 +27,14 @@ export class LoginScreenComponent implements OnInit{
   }
 
   public showPassword(){
-
+    if(this.type == "password"){
+      $("#show-password").addClass("show-password");
+      $("#show-password").attr("data-password", true);
+      this.type = "text";
+    }else if(this.type == "text"){
+      $("#show-password").removeClass("show-password");
+      $("#show-password").attr("data-password", false);
+      this.type = "password";
+    }
   }
 }
